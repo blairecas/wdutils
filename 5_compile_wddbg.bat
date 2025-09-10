08b1316e8aceb9bbefae778dc6713655329d659f
blairecas <chaminealex@mail.ru>
2025-09-10 09:22:18 +0300

    wddbg

---
+++ 5_compile_wddbg.bat
@@ -0,0 +1,18 @@
+@echo off
+
+rem compile 
+php -f ../scripts/preprocess.php wddbg.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+..\scripts\macro11 -ysl 32 -yus -m ..\scripts\sysmac.sml -l _wddbg.lst _wddbg.mac
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+php -f ../scripts/lst2bin.php _wddbg.lst release/wddbg.sav sav
+if %ERRORLEVEL% NEQ 0 ( exit /b )
+
+..\scripts\rt11dsk.exe d release\diska.dsk wddbg.sav >NUL
+..\scripts\rt11dsk.exe a release\diska.dsk release\wddbg.sav >NUL
+
+rem remove not needed files
+del _wddbg.mac
+del _wddbg.lst
+
+emulator\ukncbtl
